d7f440a033ea3144104425596f1ea54a58366887
베프 <lshqqytiger@naver.com>
2021-04-29 00:00:18 +0900

    5.8.9

--- Server/lib/Game/bot.js
+++ Server/lib/Game/bot.js
@@ -15,15 +15,36 @@ String.prototype.replaceAll = function(org, dest) {
     return this.split(org).join(dest);
 }
 exports.ban = function(target, judge, reason, bannedUntil){
-	let message = new Discord.MessageEmbed().setTitle('BF끄투 차단').setDescription('```제재 대상자: '+target.id+' ('+target.nickname+')\n'+'제재 사유: '+reason+'\n'+'처리자: '+(judge ? judge : "알 수 없음" )+'\n'+'처리 결과: 계정 정지 ('+(bannedUntil == 'permanent' ? '영구' : bannedUntil)+')```');
+	let message = new Discord.MessageEmbed()
+		.setTitle('BF끄투 차단')
+		.addFields(
+			{ name: "제재 대상자", value: `${target.id} (${target.nickname})` },
+			{ name: "제재 사유", value: reason },
+			{ name: "처리자", value: judge || "알 수 없음" },
+			{ name: "처리 결과", value: `계정 정지 (${bannedUntil == 'permanent' ? '영구' : bannedUntil})` }
+		)
 	if(GLOBAL.BOT_OPTIONS.ENABLED) Bot.channels.cache.get(GLOBAL.BOT_OPTIONS.CHANNELS.BAN).send(message);
 }
 exports.chatban = function(target, judge, reason, bannedUntil){
-	let message = new Discord.MessageEmbed().setTitle('BF끄투 채팅 금지').setDescription('```제재 대상자: '+target.id+' ('+target.nickname+')\n'+'제재 사유: '+reason+'\n'+'처리자: '+(judge ? judge : "알 수 없음" )+'\n'+'처리 결과: 채팅 금지 ('+(bannedUntil == 'permanent' ? '영구' : bannedUntil)+')```');
+	let message = new Discord.MessageEmbed()
+		.setTitle('BF끄투 채팅 금지')
+		.addFields(
+			{ name: "제재 대상자", value: `${target.id} (${target.nickname})` },
+			{ name: "제재 사유", value: reason },
+			{ name: "처리자", value: judge || "알 수 없음" },
+			{ name: "처리 결과", value: `채팅 금지 (${bannedUntil == 'permanent' ? '영구' : bannedUntil})` }
+		)
 	if(GLOBAL.BOT_OPTIONS.ENABLED) Bot.channels.cache.get(GLOBAL.BOT_OPTIONS.CHANNELS.BAN).send(message);
 }
-exports.warn = function(target, judge, warn, nowwarn){
-	let message = new Discord.MessageEmbed().setTitle('BF끄투 경고').setDescription('```제재 대상자: '+target._id+' ('+target.nickname+')\n'+'처리 결과: 경고 '+warn+'회 (현재 누적 '+nowwarn+'회)'+'\n'+'처리자: '+(judge ? judge : "알 수 없음" )+'```경고 사유는 처리자께서 따로 적어주시기 바랍니다.');
+exports.warn = function(target, judge, warn, newWarn){
+	let message = new Discord.MessageEmbed()
+		.setTitle('BF끄투 경고')
+		.addFields(
+			{ name: "제재 대상자", value: `${target._id} (${target.nickname})` },
+			{ name: "제재 사유", value: "(처리자 별도 기재 바람)" },
+			{ name: "처리자", value: judge || "알 수 없음" },
+			{ name: "처리 결과", value: `경고 ${warn}회 (누적 ${newWarn}회)` }
+		)
 	if(GLOBAL.BOT_OPTIONS.ENABLED) Bot.channels.cache.get(GLOBAL.BOT_OPTIONS.CHANNELS.BAN).send(message);
 }
 exports.word = function(type, theme, list){
