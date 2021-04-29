0b56a6fe0080a1f4c0315b56377eb8574fa3256b
베프 <lshqqytiger@naver.com>
2021-04-29 09:44:15 +0900

    5.8.9

--- Server/lib/Game/bot.js
+++ Server/lib/Game/bot.js
@@ -16,7 +16,8 @@ String.prototype.replaceAll = function(org, dest) {
 }
 exports.ban = function(target, judge, reason, bannedUntil){
 	let message = new Discord.MessageEmbed()
-		.setTitle('BF끄투 차단')
+		.setTitle('차단 | BFKKuTu')
+		.setColor('FF0000')
 		.addFields(
 			{ name: "제재 대상자", value: `${target.id} (${target.nickname})` },
 			{ name: "제재 사유", value: reason },
@@ -27,7 +28,8 @@ exports.ban = function(target, judge, reason, bannedUntil){
 }
 exports.chatban = function(target, judge, reason, bannedUntil){
 	let message = new Discord.MessageEmbed()
-		.setTitle('BF끄투 채팅 금지')
+		.setTitle('채팅 금지 | BFKKuTu')
+		.setColor("FFFF00")
 		.addFields(
 			{ name: "제재 대상자", value: `${target.id} (${target.nickname})` },
 			{ name: "제재 사유", value: reason },
@@ -36,14 +38,17 @@ exports.chatban = function(target, judge, reason, bannedUntil){
 		)
 	if(GLOBAL.BOT_OPTIONS.ENABLED) Bot.channels.cache.get(GLOBAL.BOT_OPTIONS.CHANNELS.BAN).send(message);
 }
-exports.warn = function(target, judge, warn, newWarn){
+exports.warn = function(target, judge, warn, newWarn, reason){
+	if(warn == 0) return;
+	let kind = warn > 0 ? "제재" : "차감";
 	let message = new Discord.MessageEmbed()
-		.setTitle('BF끄투 경고')
+		.setTitle(`경고 ${warn > 0 ? "" : "차감"}`)
+		.setColor(warn > 0 ? "FFFF00" : "00FF00")
 		.addFields(
-			{ name: "제재 대상자", value: `${target._id} (${target.nickname})` },
-			{ name: "제재 사유", value: "(처리자 별도 기재 바람)" },
+			{ name: `${kind} 대상자`, value: `${target._id} (${target.nickname})` },
+			{ name: `${kind} 사유`, value: reason || "(처리자 별도 기재 바람)" },
 			{ name: "처리자", value: judge || "알 수 없음" },
-			{ name: "처리 결과", value: `경고 ${warn}회 (누적 ${newWarn}회)` }
+			{ name: "처리 결과", value: `경고 ${Math.abs(warn)}회 ${warn > 0 ? "" : "차감 "}(누적 ${newWarn}회)` }
 		)
 	if(GLOBAL.BOT_OPTIONS.ENABLED) Bot.channels.cache.get(GLOBAL.BOT_OPTIONS.CHANNELS.BAN).send(message);
 }
